178ccf2422bb312bb8e1d3b41f0a1876c42db377
Fabio JS Pereira <fabiojspereira@gmail.com>
2022-02-15 18:14:13 -0300

    Versão 1.0.1 concluída. Erros tratados e melhorias implementadas.

--- Exercicio_Math_001.py
+++ Exercicio_Math_001.py
@@ -27,8 +27,8 @@ else:
 	numero_coluna = 9
 
 # PARTE DO PROGRAMA QUE GERA A GRADE DE VALORES DO UNIVERSO.
-controlador_001 = 0  # Responsável por imprimir os valores da lista universo.
-controlador_002 = 4  # Responsável por selecionar os valores selecionados.
+controlador_001 = 0     # Responsável por imprimir os valores da lista universo.
+controlador_002 = 4     # Responsável por selecionar os valores selecionados.
 check_numero_final = 0  # Contador que soma a quantidade de saltos.
 
 for linha in range(1, numero_linha + 1):
@@ -40,12 +40,16 @@ for linha in range(1, numero_linha + 1):
 		for coluna in range(1, numero_coluna + 1):
 			if controlador_001 <= len(universo):  # Verifica se ainda existem valores a serem exibidos.
 				if check_numero_final == quantidade_repeat:
-					#print("\033[1;31mEND\033[m")
 					break
 
 				else:
 					if universo[controlador_001] == universo[controlador_002]:
-						print("{}{:<2}{}".format("\033[1;34;40m",universo[controlador_001],"\033[m"), end="")
+						if check_numero_final+1 == quantidade_repeat:
+							print("{}{}{}".format("\033[7;30;42m",universo[controlador_001],"\033[m "), end="")
+
+						else:
+							print("{}{}{}".format("\033[34;40m",universo[controlador_001],"\033[m "), end="")
+
 						controlador_002 = universo[controlador_002]
 						controlador_002 = controlador_002 + 3
 						check_numero_final = check_numero_final + 1
@@ -57,9 +61,9 @@ for linha in range(1, numero_linha + 1):
 					controlador_001 = controlador_001 + 1
 
 		if check_numero_final == quantidade_repeat:
-			print("\033[7;30;42m<-RESULTADO\033[m")
+			print("\033[7;30;42m RESULTADO \033[m")
 
 print("\n")
 print("{:<32}{:^5}{:<2}".format("\033[1;33mRepetições solicitadas",":\033[m",quantidade_repeat))
-print("{:<32}{:^5}{:<2}".format("\033[1;33mÚltimo número descoberto",":\033[m",universo[controlador_002-4]))
+print("{:<32}{:^5}{}{:<1}{}".format("\033[1;33mÚltimo número descoberto",":\033[m","\033[7;30;42m",universo[controlador_002-4],"\033[m"))
 print("{:<32}{:^5}{:<2}".format("\033[1;33mUniverso pré carregado",":\033[m",len(universo)))
